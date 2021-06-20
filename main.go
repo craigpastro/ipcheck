c58ac56518bf622a3518e57c3f3ff869a52a13c8
Craig Pastro <siyopao@gmail.com>
2021-06-20 15:25:41 +0900

    Run updateBlocklists in a cron job (#4)

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/joho/godotenv"
+	"github.com/robfig/cron"
 )
 
 type appConfig struct {
@@ -26,6 +27,20 @@ func main() {
 
 	initDb(dbConfig)
 	defer dbPool.Close()
+
+	// Immediately initialize the blocklists in the db. Then run approximately
+	// daily after that.
+	//
+	// NOTE: In some sense adding and updating the blocklists is adding state
+	// to this service and, instead, this should probably be done in a Lambda.
+	checkError(updateBlocklists(), "error initializing the blocklists")
+	c := cron.New()
+	c.AddFunc("@every 25h3m", func() {
+		if err := updateBlocklists(); err != nil {
+			log.Printf("error updating blocklist: %v", err)
+		}
+	})
+	c.Start()
 
 	r := setupRouter(appConfig.ginMode)
 	r.Run(appConfig.serverAddr)
